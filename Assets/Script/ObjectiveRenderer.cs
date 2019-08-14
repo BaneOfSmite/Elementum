832c6c7e7bf90306980eb184fa00229f4ad155de
BaneOfSmite <BaneOfSmite@gmail.com>
2019-08-14 23:35:28 +0800

    Organised The Assets Folder

---
+++ Assets/Script/ObjectiveRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveRenderer : MonoBehaviour {
+	public PlayerBattle _PlayerBattle;
+	void Start() {
+		_PlayerBattle = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBattle>();
+	}
+	void Update() {
+		if (gameObject.name == "Fire" && _PlayerBattle.CurrentType == PlayerBattle.PlayerFilter.Water) {
+			SetInvis();
+		} else if (gameObject.name == "Water" && _PlayerBattle.CurrentType == PlayerBattle.PlayerFilter.Earth) {
+			SetInvis();
+		} else if (gameObject.name == "Earth" && _PlayerBattle.CurrentType == PlayerBattle.PlayerFilter.Air) {
+			SetInvis();
+		} else if (gameObject.name == "Air" && _PlayerBattle.CurrentType == PlayerBattle.PlayerFilter.Fire) {
+			SetInvis();
+		} else {
+			transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+			transform.GetChild(1).GetComponent<MeshRenderer>().enabled = false;
+		}
+	}
+	private void SetInvis() {
+		transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+		transform.GetChild(1).GetComponent<MeshRenderer>().enabled = true;
+	}
+}
---
+++ Assets/Script/PlayerBattle.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBattle : MonoBehaviour {
+    public enum PlayerFilter { Air, Water, Earth, Fire, Lightning, None }
+    public List<PlayerFilter> Have;
+    public Color[] FilterColor;
+    public PlayerFilter CurrentType;
+    private GameManager Manager;
+    private int cycle = 0;
+
+    void Start() {
+        Manager = GameManager.Instance;
+    }
+
+    void Update() {
+        if (CurrentType != PlayerFilter.None) {
+            GameManager.Instance.CurrentMana -= Time.deltaTime * 2.5f;
+            if (GameManager.Instance.CurrentMana <= 0) {
+                GameManager.Instance.CurrentMana = 0f;
+                cycle = 0;
+                CurrentType = Have[cycle];
+                GameManager.Instance.Filter.color = FilterColor[(byte) CurrentType];
+            }
+        } else {
+            if (GameManager.Instance.CurrentMana <= GameManager.Instance.PlayerMaxMana) {
+                GameManager.Instance.CurrentMana += Time.deltaTime;
+                if (GameManager.Instance.CurrentMana >= GameManager.Instance.PlayerMaxMana) {
+                    GameManager.Instance.CurrentMana = 100f;
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.F)) {
+            if (cycle != (Have.ToArray().Length - 1)) {
+                cycle++;
+            } else {
+                cycle = 0;
+            }
+            CurrentType = Have[cycle];
+            GameManager.Instance.Filter.color = FilterColor[(byte) CurrentType];
+        }
+    }
+
+    private void OnCollisionEnter(Collision hit) {
+        if (hit.gameObject.CompareTag("EnemyBattle")) {
+            Vector3 dir = hit.contacts[0].point - transform.position;
+            dir = (-dir + new Vector3(0, 1f, 0)).normalized;
+            GetComponent<Rigidbody>().AddForce(dir * 250);
+            Manager.PlayerHealth -= hit.gameObject.GetComponent<EnemyBattle>().Damage;
+        }
+        if (hit.gameObject.name.Equals("Fire")) {
+            if (CurrentType == PlayerFilter.Water) {
+                Destroy(hit.gameObject);
+                Have.Add(PlayerFilter.Fire);
+            }
+        } else if (hit.gameObject.name.Equals("Air")) {
+            if (CurrentType == PlayerFilter.Fire) {
+                Destroy(hit.gameObject);
+                Have.Add(PlayerFilter.Air);
+            }
+        } else if (hit.gameObject.name.Equals("Earth")) {
+            if (CurrentType == PlayerFilter.Air) {
+                Destroy(hit.gameObject);
+                Have.Add(PlayerFilter.Earth);
+            }
+        } else if (hit.gameObject.name.Equals("Water")) {
+            if (CurrentType == PlayerFilter.Earth) {
+                Destroy(hit.gameObject);
+                Have.Add(PlayerFilter.Water);
+            }
+        }
+    }
+}
