a526af1f01b6eb601830a061cf47f4d8f1d03f9b
BingXiang <bing6448@gmail.com>
2019-08-16 22:26:34 +0800

    updated game UI

--- Assets/Scenes/gameUIscripts/gameUIScript.cs
+++ Assets/Scenes/gameUIscripts/gameUIScript.cs
@@ -2,26 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class gameUIScript : MonoBehaviour {
 
-	
-	void Start () {
+    public bool isFullScreen;
+    public GameObject instructions;
+    public Animator instructAnimation;
+  
 
-		
-	}	
-	void Update () {
-		
-	}
-
-
+    private void Start()
+    {
+        instructions.SetActive(false);
+    }
     public void OnStartClick() {
         Debug.Log("startClicked");
+        //SceneManager.LoadScene(0);
+    }
+    public void OnInstructClick() {
+        instructions.SetActive(true);
+        instructAnimation.SetBool("clicked",true);
+        //play the animation
 
     }
-
+    public void backBtn() {
+        instructAnimation.SetBool("clicked",false);
+       // instructions.SetActive(false);
+        //playanimation to go back
+    }
     public void OnQuitClick() {
         Debug.Log("quitClicked");
         Application.Quit();
+    }
+
+    public void onCheckFullscreen() {
+        if (isFullScreen == true)
+        {
+            Debug.Log("fullscreenMode");
+            isFullScreen = false;
+            Screen.fullScreen = true;
+            //fullscreen mode
+        }
+        else if (isFullScreen == false)
+        {
+            Debug.Log("windowedMode");
+            isFullScreen = true;
+            Screen.fullScreen = false;
+        }
+
     }
 }
