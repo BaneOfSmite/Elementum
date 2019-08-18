23015212041a7fe53234888929e086d3d3208a62
BaneOfSmite <BaneOfSmite@gmail.com>
2019-08-19 00:45:09 +0800

    Added Jump To 2D

--- Assets/Script/PlayerBattle.cs
+++ Assets/Script/PlayerBattle.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerBattle : MonoBehaviour {
     private GameManager Manager;
     public GameObject[] Attacks;
+    public float speed;
+    public bool isGrounded;
     void Start() {
         Manager = GameManager.Instance;
     }
 
     void Update() {
+        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0) * Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.R)) {
             if (Manager.CurrentType == GameManager.PlayerFilter.Fire) {
                 Instantiate(Attacks[0], new Vector3(transform.position.x + 2, transform.position.y, transform.position.z), Quaternion.identity);
@@ -17,6 +20,16 @@ public class PlayerBattle : MonoBehaviour {
                 Instantiate(Attacks[1], transform.position, Quaternion.identity);
             }
         }
-        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0).normalized * 1f * Time.deltaTime;
+    }
+    private void OnCollisionEnter2D(Collision2D hit) {
+        if (hit.gameObject.CompareTag("Floor")) {
+            isGrounded = true;
+        }
+    }
+    void FixedUpdate() {
+        if (Input.GetAxis("Jump") > 0 && isGrounded) {
+            isGrounded = false;
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * 150);
+        }
     }
 }
