875bcf8f5dcd985dc924aa54e063406604833cd8
BaneOfSmite <BaneOfSmite@gmail.com>
2019-08-19 21:56:22 +0800

    Created Teleporter

---
+++ Assets/Script/Teleporter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+	private void OnTriggerEnter(Collider hit) {
+		if (hit.CompareTag("Player")) {
+			hit.transform.position = transform.GetChild(0).transform.position;
+		}
+	}
+}
